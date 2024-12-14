cff10dffe159d2e42110496d0799c3f062e5e0e0
Muhammad Rizky Syahroni <52042087+escurtcheon12@users.noreply.github.com>
2024-12-15 01:48:31 +0700

    Update palindrome.js

--- leetcode/nodejs/palindrome.js
+++ leetcode/nodejs/palindrome.js
@@ -12,3 +12,18 @@ var isPalindrome = function(x) {
 var isPalindrome = function(x) {
     return x?.toString().split('').reverse().join('') == x;
 };
+
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) return false; 
+        left++;
+        right--;
+    }
+
+    return true;
+};
